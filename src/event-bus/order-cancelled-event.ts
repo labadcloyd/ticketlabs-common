8a89991513e45d4f67c3e8cc13a3fcfa51eb5ed8
labadcloyd <abadcloyd@gmail.com>
2022-03-04 08:58:47 +0800

    added version to ticket and order events

--- src/event-bus/order-cancelled-event.ts
+++ src/event-bus/order-cancelled-event.ts
@@ -4,6 +4,7 @@ export default interface OrderCancelledEvent {
 	subject: Subjects.OrderCancelled,
 	data: {
 		id: string,
+		version: number,
 		ticket: {
 			id: string,
 			price: number
--- src/event-bus/order-created-event.ts
+++ src/event-bus/order-created-event.ts
@@ -4,6 +4,7 @@ export default interface OrderCreatedEvent {
 	subject: Subjects.OrderCreated,
 	data: {
 		id: string,
+		version: number,
 		status: OrderStatus,
 		userId: string,
 		expiresAt: string,
